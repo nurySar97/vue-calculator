b3800db639b538d58fffda7e02152c6b0b4e5268
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-15 20:28:42 +0200

    created intro

--- src/components/intro.vue
+++ src/components/intro.vue
@@ -1,5 +1,50 @@
 <template>
   <!-- Intro start -->
-  <section class="intro">Hello From Intro</section>
+  <section class="intro">
+    <div class="container">
+      <div class="section__inner">
+        <h1 class="intro__title">
+          <div>ОРГАНИЧЕСКОЕ</div>
+          <div>НАНО-УДОБНИЕ</div>
+          <div>ДОБРИВО</div>
+        </h1>
+      </div>
+    </div>
+  </section>
   <!-- Intro end -->
 </template>
+
+<style lang="scss">
+.intro {
+  width: 100%;
+  min-height: 449px;
+  margin-top: -30px;
+  padding-top: 30px;
+  background-image: url("./../assets/images/background_main.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+
+.intro__title {
+  width: 100%;
+  max-width: 607px;
+  padding-top: 245px;
+  margin-left: 90px;
+
+  & div {
+    color: #fff;
+    font-size: 3.75rem;
+    font-weight: 400;
+    line-height: 0.9;
+
+    &:nth-child(2) {
+      padding-left: 110px;
+    }
+
+    &:nth-child(3) {
+      padding-left: 160px;
+    }
+  }
+}
+</style>
