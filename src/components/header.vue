37fc438dd22c3723565668dfd94d281ab272562b
Muhammed Saryyew <nsaryyev9797@gmail.com>
2021-11-15 18:52:35 +0200

    header was created

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,7 +1,63 @@
 <template>
   <!-- Header start -->
-  <header class="header">Hello From Header</header>
+  <header class="header">
+    <div class="container">
+      <div class="header__inner">
+        <div class="header__item">
+
+          <div class="header__logo">ДОБРИВО</div>
+
+          <nav class="header__nav">
+            <ul class="header__nav-list">
+              <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Оплата и доставка</a>
+              </li>
+              <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Новости</a>
+              </li>
+              <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Справочник</a>
+              </li>
+              <li class="header__nav-item">
+                <a href="#" class="header__nav-link">Сертификаты</a>
+              </li>
+            </ul>
+            <div class="header__lang">
+              <button class="header__lang-btn">RU</button>
+            </div>
+          </nav>
+
+        </div>
+
+        <div class="header__item">
+          <nav class="header__menu">
+            <ul class="header__menu-list">
+              <li class="header__menu-item">
+                <a href="#" class="header__menu-link">НАГРО-УДОБРЕНИЕ</a>
+              </li>
+              <li class="header__menu-item">
+                <a href="#" class="header__menu-link">БиоДизель</a>
+              </li>
+              <li class="header__menu-item">
+                <a href="#" class="header__menu-link">СИЛКО</a>
+              </li>
+              <li class="header__menu-item">
+                <a href="#" class="header__menu-link">Интернет-магазин</a>
+              </li>
+              <li class="header__menu-item">
+                <a href="#" class="header__menu-link">Вопросы-ответы</a>
+              </li>
+              <li class="header__menu-item">
+                <a href="#" class="header__menu-link">Контакты</a>
+              </li>
+              <li class="header__menu-item">
+                <button class="header__menu-btn">Корзина</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
   <!-- Header end -->
 </template>
-
-<style></style>
